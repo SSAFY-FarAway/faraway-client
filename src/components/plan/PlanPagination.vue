a772395aef399e926465f43c77bf3dfdecfe8473
wjs_5025 <wjs_5025@naver.com>
2023-05-16 19:56:20 +0900

    FAR-95/feat : 여행계획 상세조회 지도

--- src/components/plan/PlanPagination.vue
+++ src/components/plan/PlanPagination.vue
@@ -1,44 +1,44 @@
 <template>
-    <nav aria-label="Page navigation">
-        <ul class="pagination justify-content-center">
-            <li class="page-item">
-            <a class="page-link" href="#" aria-label="Previous">
-                <span aria-hidden="true">&laquo;</span>
-            </a>
-            </li>
-            <!-- TODO : 토탈 카운트(totalCnt)에 따라서 개수 보여주기 -->
-            <li class="page-item"><a class="page-link" @click='movePage'>1</a></li>
-            <li class="page-item"><a class="page-link" @click='movePage'>2</a></li>
-            <li class="page-item"><a class="page-link" @click='movePage'>3</a></li>
-            <li class="page-item">
-            <a class="page-link" href="#" aria-label="Next">
-                <span aria-hidden="true">&raquo;</span>
-            </a>
-            </li>
-        </ul>
-    </nav>
+  <nav aria-label="Page navigation">
+    <ul class="pagination justify-content-center">
+      <li class="page-item">
+        <a class="page-link" href="#" aria-label="Previous">
+          <span aria-hidden="true">&laquo;</span>
+        </a>
+      </li>
+      <!-- TODO : 토탈 카운트(totalCnt)에 따라서 개수 보여주기 -->
+      <li class="page-item"><a class="page-link" @click="movePage">1</a></li>
+      <li class="page-item"><a class="page-link" @click="movePage">2</a></li>
+      <li class="page-item"><a class="page-link" @click="movePage">3</a></li>
+      <li class="page-item">
+        <a class="page-link" href="#" aria-label="Next">
+          <span aria-hidden="true">&raquo;</span>
+        </a>
+      </li>
+    </ul>
+  </nav>
 </template>
 
 <script>
 export default {
-    name: 'PlanPagination',
-    components: {},
-    props: {
-        totalCnt : Number
+  name: "PlanPagination",
+  components: {},
+  props: {
+    totalCnt: Number,
+  },
+  data() {
+    return {
+      message: "",
+    };
+  },
+  created() {
+    console.log(this.totalCnt);
+  },
+  methods: {
+    movePage(e) {
+      this.$router.push(`${this.$route.path}?page=${e.target.text}`);
     },
-    data() {
-        return {
-            message: '',
-        };
-    },
-    created() {
-        console.log(this.totalCnt)
-    },
-    methods: {
-        movePage(e) {
-            this.$router.push(`${this.$route.path}?page=${e.target.text}`)
-        }
-    },
+  },
 };
 </script>
 
