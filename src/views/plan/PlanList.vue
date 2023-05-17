c6a96688fd86255e1aa343d1eecf0c715f83af9e
wjs5025 <wjs_5025@naver.com>
2023-05-17 09:46:49 +0900

    fix : html 수정

--- src/views/plan/PlanList.vue
+++ src/views/plan/PlanList.vue
@@ -4,19 +4,15 @@
             <div class="text-center fw-bold" role="alert">
                 <h2 class="section-heading text-uppercase">Travel Plan</h2>
                 <h3 class="section-subheading text-muted">원하는 여행경로를 공유해보세요.</h3>
-                <div class="row align-self-center mb-2">
-                    <div class="col-md-2 text-start">
-                        <button type="button"  class="btn btn-primary btn shadow-sm">ADD PLAN</button>
-                    </div>
-                    <div class="col-md-7 offset-3">
-                        <form class="d-flex" id="search-form">
-                            <b-form-select id='search-select-box' v-model="selected" :options="options"></b-form-select>                          
-                            <div class="input-group shadow-sm">
-                                <input type="text" class="form-control" placeholder="검색어 입력" />
-                                <button class="btn btn-secondary" id="search-btn" type="button">Search</button>
-                            </div>
-                        </form>
-                    </div>
+                <div class="p-0 pb-1 m-0 col-12 row justify-content-between">
+                    <router-link to="/plan/write" type="button" class="btn btn-primary btn">ADD PLAN</router-link>
+                    <form class="d-flex justify-content-end">
+                        <div class="input-group col-10 p-0">
+                            <b-form-select class='col-md-4' id='search-select-box' v-model="selected" :options="options"></b-form-select>                          
+                            <input type="text" class="form-control" placeholder="검색어 입력" />
+                            <button class="btn btn-secondary ml-1" id="search-btn" type="button">Search</button>
+                        </div>
+                    </form>
                 </div>
                 <!-- 여행경로 List - 테이블 -->
                 <table class="table table-hover shadow rounded" id="plan-table">
