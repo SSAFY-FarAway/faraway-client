d06217f32700797ccfa5a1e6ad92334af10e8f6e
wjs5025 <wjs_5025@naver.com>
2023-05-17 11:30:31 +0900

    refactor : UI 변경

--- src/views/plan/PlanDetail.vue
+++ src/views/plan/PlanDetail.vue
@@ -1,5 +1,20 @@
 <template>
   <div>
+    <!-- 지도 영역 -->
+    <div class="flex" id="article-list-section" style="max-width:100vw">
+        <div class="row p-0 m-0">
+            <!-- 사용자 추천 경로 -->
+            <div class="col-6 m-0 p-0 text-center">
+                <plan-kakao-map mapId='recommand-map' :attractions='plan.attractionResponses'/>
+                <h4 class="mt-2">My Plan</h4>
+            </div>
+            <!-- 최단 경로 -->
+            <div class="col-6 m-0 p-0 text-center">
+                <plan-kakao-map mapId='shortest-map' :attractions='plan.shortestPath'/>
+                <h4 class="mt-2">Shortest Plan</h4>
+            </div>
+        </div>
+    </div>
     <div class="container">
         <section class="page-section p-3 mt-3" id="share-plan-view">
             <div class="text-center fw-bold" role="alert">
@@ -10,7 +25,6 @@
                 <hr/>
                 <!-- 게시글 영역 -->
                 <div class="col-md-12">
-                    
                     <div class="mb-3">
                         <h5 class=" d-flex justify-content-start">
                             제목
@@ -25,21 +39,7 @@
                     </div>
                 </div>
                 <hr/>
-                <!-- 지도 영역 -->
-                <div class="flex" id="article-list-section">
-                    <div class="row  justify-content-center align-self-center mb-2">
-                        <!-- 사용자 추천 경로 -->
-                        <div class="col-6">
-                            <div class="h4">My Plan</div>
-                            <plan-kakao-map mapId='recommand-map' :attractions='plan.attractionResponses'/>
-                        </div>
-                        <!-- 최단 경로 -->
-                        <div class="col-6">
-                            <div class="h4">Shortest Plan</div>
-                            <plan-kakao-map mapId='shortest-map' :attractions='plan.shortestPath'/>
-                        </div>
-                    </div>
-                </div>
+                
             </div>
     </section>
 </div>
@@ -58,9 +58,20 @@ export default {
     return {
         plan: {},
     };
-  },
+    },
+    watch: {
+        $route(to, from) {
+            if (to.path != from.path) {
+                this.getPlanData();
+            }
+        }
+    },
     created() {
-        const planId = this.$route.params.planId;
+        this.getPlanData();
+  },
+    methods: {
+        getPlanData() {
+            const planId = this.$route.params.planId;
         http.get(`/plan/${planId}`).then(res => {
             console.log(res)
             if (res.status === 200) {
@@ -69,8 +80,8 @@ export default {
         }).catch(() => {
             this.$alertDanger("데이터 로드 실패 !", "추후에 예외처리 로직 추가 예정")
         })
+    }
   },
-  methods: {},
 };
 </script>
 
--- src/views/plan/PlanWrite.vue
+++ src/views/plan/PlanWrite.vue
@@ -1,32 +1,67 @@
 <template>
-  <div>
-    <div class="container">
-  
-</div>
-  </div>
+    <div>
+      <!-- 지도 영역 -->
+      <div class="flex " id="article-list-section">
+        <div class="row m-0 p-0">
+            <!-- 사용자 추천 경로 -->
+            <div class="col-12 m-0 p-0 text-center">
+                <plan-kakao-map mapId='write-map' :attractions='attractionPath'/>
+            </div>
+        </div>
+    </div>
+      <section class="container my-5" id="share-plan-view">
+        <div class="text-center fw-bold" role="alert">
+            <h2 class="row d-flex justify-content-around my-4" >
+                Write Travle Plan
+            </h2>
+            <h4 class="section-heading text-uppercase">'memberLoginId' 님이 추천하는 여행계획입니다</h4>
+            <hr/>
+            <!-- 게시글 영역 -->
+            <div class="col-md-12">
+                
+                <div class="mb-3">
+                    <h5 class=" d-flex justify-content-start">
+                        제목
+                    </h5>
+                    <input class="form-control" type="text" :value='plan.title'>
+                </div>
+                <div>
+                    <h5 class=" d-flex justify-content-start">
+                        내용
+                    </h5>
+                    <textarea class="form-control" id="" rows="3"  :value='plan.content'></textarea>
+                </div>
+            </div>
+            <hr/>
+            
+        </div>
+        <div class="d-flex justify-content-center">
+          <b-button class="col-6 mr-1" variant="secondary" >CANCLE</b-button>
+          <b-button class="col-6 ml-1" variant="primary">WRITE</b-button>
+        </div>
+      </section>
+    </div>
 </template>
 <script>
-import http from '@/utils/api/http';
+import PlanKakaoMap from '@/components/plan/PlanKakaoMap';
 
 export default {
   name: "PlanWrite",
-    components: {
+  components: {
+    PlanKakaoMap
     },
   data() {
     return {
-        plan: {},
+      plan: {
+        title: '',
+        content: '',
+        travelPlan : []
+      },
+      attractionPath: []
     };
   },
     created() {
-        const planId = this.$route.params.planId;
-        http.get(`/plan/${planId}`).then(res => {
-            console.log(res)
-            if (res.status === 200) {
-                this.plan = res.data;
-            }
-        }).catch(() => {
-            this.$alertDanger("데이터 로드 실패 !", "추후에 예외처리 로직 추가 예정")
-        })
+       
   },
   methods: {},
 };
