9f5bc2b630e7667af84c502c8dd4cb66c10ae7b4
wjs_5025 <wjs_5025@naver.com>
2023-05-21 01:36:21 +0900

    refactor : 공통 컴포넌트 적용

---
+++ src/components/common/page/tableRowData.vue
@@ -0,0 +1,26 @@
+<template>
+  <tr class="text-center" @click="moveDetail">
+    <td v-for="title in titles" :key="title.title">
+      {{ post[title.colName] }}
+    </td>
+  </tr>
+</template>
+
+<script>
+export default {
+  name: "PostTableRow",
+  components: {},
+  props: {
+    titles: Array,
+    post: Object,
+    domain: String,
+  },
+  methods: {
+    moveDetail() {
+      this.$router.push(`/${this.domain}/view/${this.post.id}`);
+    },
+  },
+};
+</script>
+
+<style scoped></style>
--- src/views/hotplace/HotPlaceList.vue
+++ src/views/hotplace/HotPlaceList.vue
@@ -3,17 +3,34 @@
     <section class="" id="share-plan-list">
       <div class="text-center fw-bold" role="alert">
         <h2 class="section-heading text-uppercase">Hot Place</h2>
-        <h3 class="section-subheading text-muted">여행지의 핫플레이스를 공유해보세요.</h3>
+        <h3 class="section-subheading text-muted">
+          여행지의 핫플레이스를 공유해보세요.
+        </h3>
         <div class="row align-self-center mb-2">
           <div class="col-md-2 text-start">
-            <router-link class="btn btn-primary btn shadow-sm" to="/hotplace/write">글쓰기</router-link>
+            <router-link
+              class="btn btn-primary btn shadow-sm"
+              to="/hotplace/write"
+              >글쓰기</router-link
+            >
           </div>
           <div class="col-md-7 offset-3">
             <form class="d-flex" id="search-form">
-              <b-form-select id='search-select-box' v-model="selected" :options="options"></b-form-select>
+              <b-form-select
+                id="search-select-box"
+                v-model="selected"
+                :options="options"
+              ></b-form-select>
               <div class="input-group shadow-sm">
-                <input type="text" class="form-control" v-model="keyword" placeholder="검색어 입력"/>
-                <button class="btn btn-secondary" @click="search" type="button">Search</button>
+                <input
+                  type="text"
+                  class="form-control"
+                  v-model="keyword"
+                  placeholder="검색어 입력"
+                />
+                <button class="btn btn-secondary" @click="search" type="button">
+                  Search
+                </button>
               </div>
             </form>
           </div>
@@ -21,22 +38,22 @@
         <!-- 핫플레이스 List - 테이블 -->
         <table class="table table-hover shadow rounded" id="plan-table">
           <thead>
-          <tr class="text-center table-warning">
-            <th scope="col">글번호</th>
-            <th scope="col">제목</th>
-            <th scope="col">작성자</th>
-            <th scope="col">조회수</th>
-            <th scope="col">주소</th>
-            <th scope="col">평점</th>
-            <th scope="col">작성일</th>
-          </tr>
+            <tr class="text-center table-warning">
+              <th scope="col">글번호</th>
+              <th scope="col">제목</th>
+              <th scope="col">작성자</th>
+              <th scope="col">조회수</th>
+              <th scope="col">주소</th>
+              <th scope="col">평점</th>
+              <th scope="col">작성일</th>
+            </tr>
           </thead>
           <tbody v-for="hotPlace in hotPlaces" :key="hotPlace.id">
-          <hot-place-table-row :hot-place='hotPlace'/>
+            <hot-place-table-row :hot-place="hotPlace" />
           </tbody>
         </table>
         <!-- 페이지네이션 -->
-        <hot-place-pagination :totalCnt="hotPlaces.totalCnt"/>
+        <hot-place-pagination :totalCnt="hotPlaces.totalCnt" />
       </div>
     </section>
   </div>
@@ -51,45 +68,44 @@ export default {
   name: "HotPlaceList",
   components: {
     HotPlacePagination,
-    HotPlaceTableRow
+    HotPlaceTableRow,
   },
   data() {
     return {
       selected: null,
       keyword: "",
       options: [
-        {value: null, text: "검색 조건"},
-        {value: "title", text: "제목"},
-        {value: "content", text: "내용"}
+        { value: null, text: "검색 조건" },
+        { value: "title", text: "제목" },
+        { value: "content", text: "내용" },
       ],
+
       hotPlaces: [],
     };
   },
   created() {
-    http
-        .get("/hotplace")
+    http.get("/hotplace").then((response) => {
+      if (response.status === 200) {
+        console.log(response);
+        this.hotPlaces = response.data.data;
+      }
+    });
+  },
+  methods: {
+    search() {
+      let url = `/hotplace?${this.selected}=${this.keyword}`;
+      http
+        .get(url)
         .then((response) => {
           if (response.status === 200) {
             console.log(response);
             this.hotPlaces = response.data.data;
           }
+        })
+        .catch(() => {
+          this.$alertDanger("검색 실패", "예외 처리 추가 예정");
         });
-  },
-  methods: {
-    search() {
-      let url = `/hotplace?${this.selected}=${this.keyword}`
-      http
-          .get(url)
-          .then((response) => {
-            if (response.status === 200) {
-              console.log(response);
-              this.hotPlaces = response.data.data;
-            }
-          })
-          .catch(() => {
-            this.$alertDanger("검색 실패", "예외 처리 추가 예정");
-          });
-    }
+    },
   },
 };
 </script>
