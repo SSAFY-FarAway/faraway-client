30b134e954d39654289c2af1eef18cde8de9a7f5
wjs_5025 <wjs_5025@naver.com>
2023-05-21 00:06:38 +0900

    refactor : 불필요한 코드 제거

--- src/components/common/page/pageHeader.vue
+++ src/components/common/page/pageHeader.vue
@@ -1,13 +1,12 @@
 <template>
-  <div>
-    <div class="text-center fw-bold" role="alert">
-      <h2 class="row d-flex justify-content-around my-4 text-uppercase">
-        {{ title }}
-      </h2>
-      <h5 class="section-subheading text-muted">{{ subTitle }}</h5>
-    </div>
+  <div class="text-center">
+    <h2 class="my-4 text-uppercase">
+      {{ title }}
+    </h2>
+    <h5 class="text-muted">{{ subTitle }}</h5>
   </div>
 </template>
+
 <script>
 export default {
   name: "pageHeader",
@@ -15,13 +14,5 @@ export default {
     title: String,
     subTitle: String,
   },
-  components: {},
-  data() {
-    return {};
-  },
-  created() {},
-  methods: {},
 };
 </script>
-
-<style scoped></style>
