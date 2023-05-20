52c7b95bd29bbc1766d2b34dedda9d8dff52984e
wjs_5025 <wjs_5025@naver.com>
2023-05-21 00:55:19 +0900

    add : 공통 컴포넌트 파일 추가

---
+++ src/components/common/page/tableRowHeader.vue
@@ -0,0 +1,20 @@
+<template>
+  <tr class="text-center table-warning">
+    <th
+      v-for="title in titles"
+      :key="title.title"
+      :class="'col-' + title.colValue"
+    >
+      {{ title.title }}
+    </th>
+  </tr>
+</template>
+
+<script>
+export default {
+  name: "tableRowHeader",
+  props: {
+    titles: Array,
+  },
+};
+</script>
---
+++ src/components/common/page/writeBtn.vue
@@ -0,0 +1,18 @@
+<template>
+  <router-link
+    type="button"
+    class="btn btn-primary btn text-uppercase"
+    :to="path"
+    >{{ btnName }}
+  </router-link>
+</template>
+
+<script>
+export default {
+  name: "writeBtn",
+  props: {
+    btnName: String,
+    path: String,
+  },
+};
+</script>
