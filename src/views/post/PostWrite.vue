de66ab514e88514c3bd2726c5accda1fcf3b5aec
wjs5025 <wjs_5025@naver.com>
2023-05-22 15:33:16 +0900

    fix : 버튼 수정

--- src/views/post/PostWrite.vue
+++ src/views/post/PostWrite.vue
@@ -8,11 +8,11 @@
   </b-form-select>
     <!-- 게시글 제목 -->
     <div class="row m-0 mt-3">
-      <input type="text" class="form-control font-weight-bold " id="title" style="font-size : 1.2em" v-model="request.title" placeholder="제목을 입력해주세요."/>
+      <input type="text" class="form-control " id="title" style="font-size : 1.1em" v-model="request.title" placeholder="제목을 입력해주세요"/>
     </div>
 
     <!-- 게시글 정보 -->
-    <div class="mt-2">
+    <div class="mt-3">
       <span id="member-id" class="text-secondary fw-light" style="font-size : 0.9em">
         작성자 : {{ loginMember.loginId }} 
       </span>
@@ -23,11 +23,11 @@
     </div>
 
     <!-- 게시글 내용 -->
-    <textarea class="form-control mt-3" id="content" v-model="request.content" rows="7" placeholder="내용을 입력해주세요.">
+    <textarea class="form-control mt-3" id="content" v-model="request.content" rows="7" placeholder="내용을 입력해주세요">
     </textarea>
 
     <!-- 파일 첨부 영역 -->
-    <div class='mt-3 '>
+    <div class='mt-3'>
       <p class="font-weight-bold">첨부파일</p>
       <input style="cursor:pointer" type="file" @change="selectFile" ref="uploadFile" multiple/>
     </div>
@@ -36,9 +36,9 @@
 
     <!-- 게시글 하단 메뉴 -->
     <div class="row p-0 m-0 justify-content-end">
-      <router-link to="/post/list" class="btn btn-secondary">
-        목록으로
-      </router-link>
+      <button @click="$router.go(-1)" class="btn btn-secondary">
+        이전으로
+      </button>
       <button class="btn btn-primary ml-2" @click="write">글 작성</button>
     </div>
    
